181bc238bba603413a92def150d664697788d748
bkushigian <bkushigian@gmail.com>
2024-08-27 12:45:19 -0700

    Skipping pio-based tests

--- tests/pious/pio/test_blockers.py
+++ tests/pious/pio/test_blockers.py
@@ -1,10 +1,13 @@
+import pytest
+import os
 from pious.pio.blockers import compute_single_card_blocker_effects
 from pious.pio.util import make_solver
 
-s = make_solver()
-s.load_tree(r"F:\Database\SimpleTree\SRP\Range b25\BTNvBB\5c4d3c.cfr")
 
-
+@pytest.mark.skipif(os.name != "nt", reason="Only runs on Windows")
 def test_blocker_effects():
+    s = make_solver()
+    s.load_tree(r"F:\Database\SimpleTree\SRP\Range b25\BTNvBB\5c4d3c.cfr")
+
     effects = compute_single_card_blocker_effects(s, "r:0:c")
     print(effects)
