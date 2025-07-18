38ab0b780afe80834fb1ca9dc8efa979fb9d7f31
bkushigian <bkushigian@gmail.com>
2025-07-17 21:44:15 -0700

    Added todo for blockers test (currently is bad!)

--- tests/pious/pio/test_blockers.py
+++ tests/pious/pio/test_blockers.py
@@ -1,5 +1,7 @@
 import pytest
 import os
+from pious.hands import Hand
+from pious.pio.aggregate import SpotData
 from pious.pio.blockers import compute_single_card_blocker_effects
 from pious.pio.util import make_solver
 import importlib.resources
@@ -8,6 +10,7 @@ cfr_db_path = importlib.resources.files("pious.pio.resources.database")
 cfr_path = cfr_db_path / "2c2s2d.cfr"
 
 
+# TODO: This is not a good test!
 @pytest.mark.skipif(os.name != "nt", reason="Only runs on Windows")
 def test_blocker_effects():
     s = make_solver()
